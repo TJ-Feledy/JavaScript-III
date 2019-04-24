4542bfba06663869779c4a42dbb185a9de4eb52e
teej387 <teej387@yahoo.com>
2019-04-23 19:43:58 -0500

    Created GameObject, CharacterStats, and Humanoid Constructor Functions

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,14 +15,28 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
-
+function GameObject(attribute) {
+  this.createdAt = attribute.createdAt;
+  this.name = attribute.name;
+  this.dimensions = attribute.dimensions;
+}
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game.`;
+}
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
-
+function CharacterStats(characterAttribute) {
+  GameObject.call(this, characterAttribute);
+  this.healthPoints = characterAttribute.healthPoints;
+}
+CharacterStats.prototype.takeDamage = function () {
+  return `${this.name} took damage.`;
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
@@ -32,7 +46,17 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+ function Humanoid(humanoidAttribute) {
+   CharacterStats.call(this, humanoidAttribute);
+   GameObject.call(this,humanoidAttribute);
+   this.team = humanoidAttribute.team;
+   this.weapons = humanoidAttribute.weapons;
+   this.language = humanoidAttribute.language;
+ }
+ Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}.`;
+ }
+ Humanoid.prototype = Object.create(CharacterStats.prototype);
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
